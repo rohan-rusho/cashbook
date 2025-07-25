3e7bfe6a9d67e354b75ae40711fcd35754ac2c45
Md. Rohan Islam <111059342+rohan-rusho@users.noreply.github.com>
2025-07-25 16:14:47 +0600

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,12 +1,6 @@
 // Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyCVvr3Ku5hMh_bOd4LwFJc661TL6nPURKk",
-    authDomain: "cashbook-5b70b.firebaseapp.com",
-    databaseURL: "https://cashbook-5b70b-default-rtdb.firebaseio.com",
-    projectId: "cashbook-5b70b",
-    storageBucket: "cashbook-5b70b.firebasestorage.app",
-    messagingSenderId: "149737654561",
-    appId: "1:149737654561:web:fc69ce1b9dbc187dd09660"
+   
 };
 
 // Make config available globally for compatibility
